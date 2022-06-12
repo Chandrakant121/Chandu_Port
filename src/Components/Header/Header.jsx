7d2418e4b653e1ebb90df2d04577da3fe08f8907
Chandrakant121 <csjoshi989@gmail.com>
2022-06-12 18:44:45 +0530

    updated

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navbar } from "../Navbar/Navbar";
 import { ThemeContext } from "../../Context/theme";
-import Image from "./../../assets/logo.png";
+import Image from "./../../assets/logo.jpg";
 
 import "./Header.css";
 export const Header = () => {
